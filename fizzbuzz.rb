00e005e7526b5a3154f5ce81a4bec0c9c5655513
ssstroud23 <ssstroud23@gmail.com>
2018-07-11 15:25:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def FizzBuzz(int)
-  if int % 3 
+  if int % 3 == 0
     puts "Fizz"
-  elsif int % 5
+  elsif int % 5 == 0
     puts "Buzz"
-  else int % 15
+  else int % 15 == 0 
     puts "FizzBuzz"
   
 end
