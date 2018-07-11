597122b7e9499879d99621712490a56bdc712512
ssstroud23 <ssstroud23@gmail.com>
2018-07-11 15:36:35 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(int)
+  if int % 15 == 0 
+    puts "FizzBuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  else   
+    puts "nil"
+  
+end
+
+end
+
+fizzbuzz(30)
